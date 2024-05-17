b585bf7b2492eea9139ce82c6d1eb45fcc579de3
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-05-16 19:45:41 -0700

    add new icon to google tab

--- liftLogFrontend/src/App.tsx
+++ liftLogFrontend/src/App.tsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
