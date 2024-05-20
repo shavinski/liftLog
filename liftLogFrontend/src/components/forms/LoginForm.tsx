2e033536a0af3e442d059c09766081818c19e347
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-05-20 16:16:34 -0700

    Fix log in button
    Add onSubmit to the actual form vs button

--- liftLogFrontend/src/components/forms/LoginForm.tsx
+++ liftLogFrontend/src/components/forms/LoginForm.tsx
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
 
+interface FormData {
+    email: string;
+    password: string;
+}
+
 function LoginForm() {
-    const [formData, setFormData] = useState({
+    const [formData, setFormData] = useState<FormData>({
         email: "",
         password: "",
     });
 
-    const handleChange = (e) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
         setFormData((prevState) => ({ ...prevState, [name]: value }));
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        console.log("Sending to server...");
+        console.log(formData);
         // TODO: SEND FORM INFO TO BACKEND LATER ON
     }
 
@@ -20,29 +27,43 @@ function LoginForm() {
         <div className="flex flex-col justify-center items-center md:max-w-lg md:mx-auto mt-12">
             <p className="text-lg font-bold p-4">Account Login</p>
 
-            <form className="flex flex-col w-11/12">
+            <form className="flex flex-col w-11/12" onSubmit={handleSubmit}>
                 {/* EMAIL INPUT */}
                 <div>
-                    <label className="absolute bg-white ml-4 text-md" htmlFor="email">Email Address</label>
+                    <label
+                        className="absolute bg-white ml-4 text-md"
+                        htmlFor="email">
+                        Email Address
+                    </label>
                     <input
                         value={formData.email} onChange={handleChange}
                         className="mt-3 p-3 text-lg w-full border-solid border-4 border-light-grey-500"
                         type="email"
-                        name="email" />
+                        name="email"
+                        id="email" />
                 </div>
 
                 {/* PASSWORD INPUT */}
                 <div className="mt-3">
-                    <label className="absolute bg-white ml-4 text-md" htmlFor="email">Password</label>
+                    <label
+                        className="absolute bg-white ml-4 text-md"
+                        htmlFor="password">
+                        Password
+                    </label>
                     <input
                         value={formData.password} onChange={handleChange}
                         className="mt-3 p-3 text-lg w-full border-solid border-4 border-light-grey-500"
                         type="password"
-                        name="password" />
+                        name="password"
+                        id="password" />
                 </div>
 
-
-                <button onClick={handleSubmit} className="mt-10 h-12 rounded-md bg-[#00df9a]">Log In</button>
+                {/* SUBMIT BUTTON */}
+                <button
+                    type="submit"
+                    className="mt-10 h-12 rounded-md bg-[#00df9a]">
+                    Log In
+                </button>
             </form>
 
             <small className="text-center m-5">Not a member? <a className="text-blue-700" href="/account/create">Sign up</a> here!</small>
