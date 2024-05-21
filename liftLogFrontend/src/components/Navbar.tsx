5cc64bcca9e9355f6b445a3885d2e443365f1c6b
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-05-20 19:07:12 -0700

    Fix z index issue on mobile nav menu

--- liftLogFrontend/src/components/Navbar.tsx
+++ liftLogFrontend/src/components/Navbar.tsx
@@ -39,8 +39,8 @@ const Navbar = () => {
             <ul
                 className={
                     nav
-                        ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
-                        : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+                        ? 'z-50 fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
+                        : 'z-50 ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
                 }
             >
                 {/* Mobile Logo */}
