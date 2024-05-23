4ecc4f251ef84dcd456aec73d2f5d2031aa2d459
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-05-23 13:23:23 -0700

    Switch back to radio buttons
    Need to figure out error with my state

--- liftLogFrontend/src/components/forms/SignupForm/GoalContainer.tsx
+++ liftLogFrontend/src/components/forms/SignupForm/GoalContainer.tsx
@@ -1,13 +1,35 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 interface GoalContainerProps {
     goal: string;
 }
 
 const GoalContainer: FC<GoalContainerProps> = ({ goal }) => {
+    const [activeGoal, setActiveGoal] = useState<string>("");
+
+    const changeActiveGoal = (e: React.MouseEvent<HTMLLabelElement>) => {
+        const target = e.target as HTMLButtonElement;
+        const goalValue = target.value;
+
+        setActiveGoal(goalValue);
+    };
+
     return (
         <>
-            <h1>{goal}</h1>
+            {/* <button
+                type="button"
+                className={`mt-5 p-5 border-solid border-4 rounded-lg ${activeGoal === goal ? 'border-green-500' : 'border-light-grey-500'}`}
+                onClick={changeActiveGoal}
+                value={goal}>
+                {goal}
+            </button> */}
+
+            <label onClick={changeActiveGoal} htmlFor={goal}>
+                <div className={`mt-5 p-5 border-solid border-4 rounded-lg ${activeGoal === goal ? 'border-green-500' : 'border-light-grey-500'}`}>
+                    <span className="text-lg">{goal}</span>
+                    <input className="hidden" type="radio" id={goal} name="goal" value={goal} />
+                </div>
+            </label>
         </>
     )
 }
