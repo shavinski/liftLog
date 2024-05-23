3118993c4a088f460996aac09476275c7a13afb2
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-05-23 15:03:52 -0700

    Get active button to work, but messed up clicking on whole container

--- liftLogFrontend/src/components/forms/SignupForm/GoalContainer.tsx
+++ liftLogFrontend/src/components/forms/SignupForm/GoalContainer.tsx
@@ -12,16 +12,20 @@ const GoalContainer: FC<GoalContainerProps> = ({ goal, handleChange }) => {
 
     return (
         <>
-            <label htmlFor={goal}>
-                <div className="mt-5 p-5 border-solid border-4 rounded-lg">
-                    <span className="text-lg">{goal}</span>
-                    <input
-                        onChange={handleChange}
-                        className="hidden"
-                        type="radio" id={goal}
-                        name="goal"
-                        value={goal} />
-                </div>
+            <label
+                className="mt-5 p-5 border-solid border-light-grey-500 border-4 rounded-lg has-[:checked]:bg-green-500 has-[:checked]:border-green-800"
+                htmlFor="userGoal">
+                {/* <div> */}
+                <span className="text-lg">{goal}</span>
+                <input
+                    onChange={handleChange}
+                    className=""
+                    type="radio"
+                    value={goal}
+                    id={goal}
+                    name="userGoal"
+                    required />
+                {/* </div> */}
             </label>
         </>
     )
