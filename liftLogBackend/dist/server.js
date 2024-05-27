3318827db7496bd062e64d8eb2071e5d6c7c8f80
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-05-26 17:30:05 -0700

    Adjust tsconfig add the app.use for 404 pages not found

--- liftLogBackend/dist/server.js
+++ liftLogBackend/dist/server.js
@@ -4,15 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const PORT = 8000;
 const app = (0, express_1.default)();
+const hostname = "localhost";
+const PORT = 3000;
 app.get("/", (req, res) => {
     res.send("Hello from me now on TS, change this");
 });
 app.get("/hi", (req, res) => {
     res.send("Test? maybe?");
 });
-app.listen(PORT, () => {
-    console.log(`Now listening on ${PORT}`);
-    console.log(`http://localhost:${PORT}`);
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry, the page you are looking for does not exist.");
 });
+app.listen(PORT, () => {
+    console.log(`\n\nServer running at http://${hostname}:${PORT}`);
+});
+//# sourceMappingURL=server.js.map
--- liftLogBackend/server.ts
+++ liftLogBackend/server.ts
@@ -1,7 +1,9 @@
-import express, {Express, Request, Response} from "express";
-const PORT = 8000;
+import express, { Express, Request, Response } from "express";
 
 const app = express();
+
+const hostname = "localhost"
+const PORT = 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello from me now on TS, change this")
@@ -11,7 +13,10 @@ app.get("/hi", (req, res) => {
     res.send("Test? maybe?")
 });
 
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry, the page you are looking for does not exist.");
+});
+
 app.listen(PORT, () => {
-    console.log(`Now listening on ${PORT}`);
-    console.log(`http://localhost:${PORT}`);
+    console.log(`\n\nServer running at http://${hostname}:${PORT}`);
 });
