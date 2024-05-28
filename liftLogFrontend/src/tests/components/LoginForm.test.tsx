dfbcd9dc371f3b48e5f93c149f6be7e573e798f8
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-05-28 10:41:35 -0700

    Add a test for change input on login form

--- liftLogFrontend/src/tests/components/LoginForm.test.tsx
+++ liftLogFrontend/src/tests/components/LoginForm.test.tsx
@@ -1,13 +1,14 @@
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, render, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import userEvent from '@testing-library/user-event'
+// import userEvent from '@testing-library/user-event'
 
 import LoginForm from '../../components/forms/LoginForm';
 import SignupForm from '../../components/forms/SignupForm/SignupForm';
 
 
 afterEach(cleanup);
-const mockUser = userEvent.setup();
+
+// const mockUser = userEvent.setup();
 
 test('renders App component', () => {
     render(
@@ -33,8 +34,23 @@ test('Makes sure the sign up here link redirects to SignUp form', () => {
     expect(signUpLink).toBeInTheDocument();
 
     // User clicks sign up link
-    mockUser.click(signUpLink);
+    fireEvent.click(signUpLink);
 
     // Redirects to Signup Form 
     expect(screen.getByText(/Account Information/i)).toBeInTheDocument();
-})
+});
+
+test('HandleChange functions properly', () => {
+    const { getByLabelText } = render(
+        <MemoryRouter>
+            <LoginForm  />
+        </MemoryRouter>
+    );
+
+    const emailInput = getByLabelText(/Email Address/);
+    fireEvent.change(emailInput, { target: { value: 'test-email' } });
+
+    expect(emailInput).toHaveValue('test-email');
+});
+
+// TODO: Create test for login functionality when I finish writing endpoint on backed 
