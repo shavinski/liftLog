1e8d2c0aa2621dd5e3358c2aa183b7e1a911f17a
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-05-31 17:13:22 -0700

    create method called createAccount
    Include docstring to detail method
    Add a duplicate checker on the username column

--- liftLogBackend/src/models/user.ts
+++ liftLogBackend/src/models/user.ts
@@ -1,5 +1,7 @@
 import db from "../db";
 
+
+
 class User {
     static async getAllUsers() {
         const result = await db.query(`
@@ -12,7 +14,29 @@ class User {
         const allUsers = result.rows;
 
         return allUsers;
-    }
+    };
+
+    /**
+        Creating a user account from the sign up form 
+        
+        Data received should be:
+        { firstName, lastName, heightFeet, heightInches, weight, bodyType, goal, username, email, password }
+
+        Check if user exists with email, then throw error 
+    */
+
+    static async createAccount({ firstName, lastName, heightFeet, heightInches, weight, bodyType, goal, username, email, password }) {
+        const checkDuplicateUser = await db.query(`
+            SELECT username 
+            FROM users
+            WHERE username = $1`, [username]
+        );
+
+        if (checkDuplicateUser.rows.length > 0) {
+            throw new Error(`User already exists: ${username}`);
+        };
+
+    };
 }
 
 export default User;
