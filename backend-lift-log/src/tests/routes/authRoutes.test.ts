0e9abba5db88a661400eeefc7a16c358c35c1687
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-12-06 12:18:11 -0800

    Add single test for testing for 400 error code on bad signup

--- backend-lift-log/src/tests/routes/authRoutes.test.ts
+++ backend-lift-log/src/tests/routes/authRoutes.test.ts
@@ -1,8 +1,14 @@
 "use strict";
 
 import app from "../../app";
-import { commonAfterAll, commonAfterEach, commonBeforeAll, commonBeforeEach } from "../testCommon";
+import {
+    commonAfterAll,
+    commonAfterEach,
+    commonBeforeAll,
+    commonBeforeEach
+} from "../testCommon";
 
+import request from "supertest";
 
 beforeAll(commonBeforeAll);
 beforeEach(commonBeforeEach);
@@ -12,9 +18,27 @@ afterAll(commonAfterAll);
 describe("Testing auth routes", () => {
 
     describe("POST /auth/signup", () => {
-        test("Wrong data returns errors", () => {
-            
-        })
-    })
+        test("Return 400 with missing information", async () => {
+            const res = await request(app)
+                .post("/auth/signup")
+                .send({
+                    "firstName": "First",
+                    "lastName": "",
+                    "heightFeet": 5,
+                    "heightInches": 10,
+                    "weight": 180,
+                    "bodyType": "ectomorph",
+                    "goal": "gain weight",
+                    "username": "test-user",
+                    "email": "test-user@gmail.com",
+                    "password": "password"
+                })
+            expect(res.status).toBe(400);
+        });
 
+        test("Return 200 with successful signup", async () => {
+
+        })
+
+    })
 })
