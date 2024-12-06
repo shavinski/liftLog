0b059b34986bc9183be8c1a38323d191b0be467f
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-12-06 09:47:21 -0800

    Download supertest
    Update dev dependencies and dependencies
    Create auth folders for tests

--- backend-lift-log/src/tests/models/user.test.ts
+++ backend-lift-log/src/tests/models/user.test.ts
@@ -1,5 +1,5 @@
-import User from "../models/user";
-import db from "../db";
+import User from "../../models/user";
+import db from "../../db";
 
 afterEach(async () => {
     try {
---
+++ backend-lift-log/src/tests/routes/authRoutes.test.ts
@@ -0,0 +1,12 @@
+"use strict";
+
+import app from "../../app";
+describe("Testing auth routes", () => {
+
+    describe("POST /auth/signup", () => {
+        test("Wrong data returns errors", () => {
+            
+        })
+    })
+
+})
