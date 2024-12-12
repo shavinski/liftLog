3e3b766defe4bef0f9ed593ba64c2dbcf56a92c6
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-12-12 11:56:39 -0800

    Add more descriptors in tests for program controller

--- backend-lift-log/src/tests/controllers/programController.test.ts
+++ backend-lift-log/src/tests/controllers/programController.test.ts
@@ -9,22 +9,35 @@ import {
 } from "../testCommon";
 
 import request from "supertest";
-import { yellowBright } from "chalk"
 
-describe(yellowBright("Endpoint: /programs"), () => {
-   
-    beforeEach(commonBeforeEach); 
+describe("Endpoint: /programs", () => {
+
+    beforeEach(commonBeforeEach);
     afterEach(commonAfterEach);
     afterAll(commonAfterAll);
 
     describe("User with no workout programs", () => {
-        it("200 with a user getting all their programs (NO PROGRAMS)", async () => {
+        it("should return 200 even if no programs exist", async () => {
             const res = await request(app)
                 .get("/programs")
                 .set("Authorization", `Bearer ${u1Token}`)
 
             expect(res.body).toMatchObject({
                 programs: []
+            });
+        });
+    });
+
+    describe("Request with no bearer token or invalid token", () => {
+        it("should return 401 with invalid token passed through", async () => {
+            const res = await request(app)
+                .get("/programs")
+                .set("Authorization", `Bearer invalidTokenPassed`)
+
+            expect(res.body).toMatchObject({
+                context: {},
+                error: "Unauthorized",
+                messages: ["Invalid or expired token"],
             });
         });
     });
